a2a2229a277be8dbc67f68f876491e28e055f9a3
Alvin Wan <hi@alvinwan.com>
2016-08-03 17:03:52 -0700

    added command, converting output to js

--- cli/constants.py
+++ cli/constants.py
@@ -7,6 +7,7 @@ SOURCE_DIR = 'data'
 TARGET_DIR = 'out'
 CLEANED_FILE_NAME = os.path.join(TARGET_DIR, 'cleaned.txt')
 OUTPUT_FILE_NAME = os.path.join(TARGET_DIR, 'output.txt')
+JS_FILE_NAME = os.path.join(TARGET_DIR, 'words.js')
 
 """
 :param letter: curse words beginning with this letter
--- cli/taboo.py
+++ cli/taboo.py
@@ -8,14 +8,15 @@ Usage:
     taboo.py expand <word> [options]
     taboo.py unmentionables <src> [options]
     taboo.py scrape (--noswearing | --dictionary) [--force] [options]
+    taboo.py tojs <src>
 
 Options:
     -h --help       Show this screen.
     -v --verbose   Print log messages and status updates
 """
 
-from constants import TARGET_DIR
 from constants import CLEANED_FILE_NAME
+from constants import JS_FILE_NAME
 from constants import OUTPUT_FILE_NAME
 from docopt import docopt
 from utils.clean import clean
@@ -52,6 +53,11 @@ def main(arguments: dict):
             verbose=arguments['--verbose'])
     elif arguments['scrape']:
         _scrape(arguments)
+    elif arguments['tojs']:
+        _tojs(
+            src=arguments['<src>'],
+            dest=JS_FILE_NAME,
+            verbose=arguments['--verbose'])
 
 
 def _clean(src: str, dest: str, verbose: bool) -> None:
@@ -101,6 +107,24 @@ def _scrape(arguments: dict):
     if arguments['--noswearing']:
         scrape('noswearing', verbose, force)
 
+def _tojs(src: str, dest: str, verbose: bool) -> None:
+    """Generate javascript file with list from output.
+
+    Specifically, creates a list of pairs, with (word, unmentionables).
+
+    :param from: the source file to read from
+    :param to: the target file to write to
+    :param verbose: whether or not print statuses
+    """
+    with open(dest, 'w') as f:
+        f.write('words = [\n')
+        for line in read_lines(src):
+            keyword, unmentionables = line.split('|')
+            new_line = '["{keyword}", ["{unmentionables}"]],\n'.format(
+                keyword=keyword.strip(),
+                unmentionables='","'.join(unmentionables.strip().split(' ')))
+            f.write(new_line)
+        f.write('];')
 
 if __name__ == '__main__':
     main(docopt(__doc__, version='Taboo 1.0'))
